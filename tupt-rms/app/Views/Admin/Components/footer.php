e93d2b628354883ae7b3114be2bd3770b3980ce0
Y4NYAN-PROJECTS <paulianjucar2020@gmail.com>
2025-02-24 17:54:02 +0800

    Remove tupt-rms folder

--- tupt-rms/app/Views/Admin/Components/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- [ Footer ] -->
-<footer class="pc-footer">
-    <div class="footer-wrapper container-fluid">
-        <p class="m-0">Roselle Honorio <small>v.12</small></p>
-    </div>
-</footer>
--- tupt-rms/app/Views/Index/Components/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="auth-footer">
-    <p class="m-0 w-100 text-center">
-        Technological University of the Philippines - Roselle Honorio <small>v.12</small>
-    </p>
-</div>
